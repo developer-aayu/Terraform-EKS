f91adca33b1ff7fa1607029b4450d9ef6254c65e
developer-aayu <aayusharma00123@gmail.com>
2024-09-15 16:03:52 +0530

    added comments to vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -3,6 +3,7 @@ provider "aws" {
 }
 
 data "aws_availability_zones" "available" {}
+# Retrieves a list of all available Availability Zones in the specified region.
 
 locals {
   cluster_name = "aayu-eks"
@@ -19,20 +20,28 @@ module "vpc" {
   private_subnets      = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets       = ["10.0.4.0/24", "10.0.5.0/24"]
 
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
-  enable_dns_hostnames = true
-  enable_dns_support   = true
+  enable_nat_gateway = true
+  single_nat_gateway = true
 
+  # Enables DNS hostnames for instances within the VPC.
+  enable_dns_hostnames = true
+
+  # Enables DNS support for the VPC, allowing domain name resolution.
+  enable_dns_support = true
+
+
+  # Marks VPC resources as shared to be used by k8s cluster
   tags = {
     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
   }
 
+  # for external loadbalancing
   public_subnet_tags = {
     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
     "kubernetes.io/role/elb"                      = "1"
   }
 
+  # for internal loadbalancing
   private_subnet_tags = {
     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
     "kubernetes.io/role/internal-elb"             = "1"
